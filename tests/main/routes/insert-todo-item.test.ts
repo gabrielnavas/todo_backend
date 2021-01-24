2e544088b81fbe925acc9779b3e9c9e1ae53ecaf
navasi <gabrielnavas@email.com>
2021-01-24 16:16:12 -0300

    updated todo item add rest integration tests

--- tests/main/routes/insert-todo-item.test.ts
+++ tests/main/routes/insert-todo-item.test.ts
@@ -42,127 +42,127 @@ describe('POST /insert_todo_item', () => {
     })
   })
 
-  // describe('Expect 400 badRequest', () => {
-  //   beforeEach(async () => {
-  //     await PGHelper.getPool().query('DELETE FROM public."user_token_access" CASCADE')
-  //     await PGHelper.getPool().query('DELETE FROM public."todo_item" CASCADE')
-  //     await PGHelper.getPool().query('DELETE FROM public."user" CASCADE')
-  //   })
+  describe('Expect 400 badRequest', () => {
+    beforeEach(async () => {
+      await PGHelper.getPool().query('DELETE FROM public."user_token_access" CASCADE')
+      await PGHelper.getPool().query('DELETE FROM public."todo_item" CASCADE')
+      await PGHelper.getPool().query('DELETE FROM public."user" CASCADE')
+    })
 
-  //   afterEach(async () => {
-  //     await PGHelper.getPool().query('DELETE FROM public."user_token_access" CASCADE')
-  //     await PGHelper.getPool().query('DELETE FROM public."todo_item" CASCADE')
-  //     await PGHelper.getPool().query('DELETE FROM public."user" CASCADE')
-  //   })
+    afterEach(async () => {
+      await PGHelper.getPool().query('DELETE FROM public."user_token_access" CASCADE')
+      await PGHelper.getPool().query('DELETE FROM public."todo_item" CASCADE')
+      await PGHelper.getPool().query('DELETE FROM public."user" CASCADE')
+    })
 
-  //   test('should return 400 on /api/signup missing idNameTodoArea', async done => {
-  //     await request(app)
-  //       .post('/api/signup')
-  //       .send({
-  //         name: 'Gabriel Navas',
-  //         email: 'gabrielnavas@gmail.com',
-  //         password: '123456',
-  //         passwordConfirmation: '123456'
-  //       })
-  //       .expect(200)
-  //       .then(async res => {
-  //         const token = res.body.token
-  //         await request(app)
-  //           .post('/api/insert_todo_item')
-  //           .set('x-access-token', token)
-  //           .send({
-  //             title: 'any_title',
-  //             description: 'any_descripasdasdsadasdtion'
-  //           })
-  //           .expect(400)
-  //         done()
-  //       })
-  //   })
+    test('should return 400 on /api/signup missing idNameTodoArea', async done => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Gabriel Navas',
+          email: 'gabrielnavas@gmail.com',
+          password: '123456',
+          passwordConfirmation: '123456'
+        })
+        .expect(200)
+        .then(async res => {
+          const token = res.body.token
+          await request(app)
+            .post('/api/insert_todo_item')
+            .set('x-access-token', token)
+            .send({
+              title: 'any_title',
+              description: 'any_descripasdasdsadasdtion'
+            })
+            .expect(400)
+          done()
+        })
+    })
 
-  //   test('should return 400 on /api/signup missing title', async done => {
-  //     await request(app)
-  //       .post('/api/signup')
-  //       .send({
-  //         name: 'Gabriel Navas',
-  //         email: 'gabrielnavas@gmail.com',
-  //         password: '123456',
-  //         passwordConfirmation: '123456'
-  //       })
-  //       .expect(200)
-  //       .then(async res => {
-  //         const token = res.body.token
-  //         await request(app)
-  //           .post('/api/insert_todo_item')
-  //           .set('x-access-token', token)
-  //           .send({
-  //             idNameTodoArea: 'any_todo_area_name',
-  //             description: 'any_descripasdasdsadasdtion'
-  //           })
-  //           .expect(400)
-  //         done()
-  //       })
-  //   })
+    test('should return 400 on /api/signup missing title', async done => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Gabriel Navas',
+          email: 'gabrielnavas@gmail.com',
+          password: '123456',
+          passwordConfirmation: '123456'
+        })
+        .expect(200)
+        .then(async res => {
+          const token = res.body.token
+          await request(app)
+            .post('/api/insert_todo_item')
+            .set('x-access-token', token)
+            .send({
+              idNameTodoArea: 'any_todo_area_name',
+              description: 'any_descripasdasdsadasdtion'
+            })
+            .expect(400)
+          done()
+        })
+    })
 
-  //   test('should return 400 on /api/signup missing description', async done => {
-  //     await request(app)
-  //       .post('/api/signup')
-  //       .send({
-  //         name: 'Gabriel Navas',
-  //         email: 'gabrielnavas@gmail.com',
-  //         password: '123456',
-  //         passwordConfirmation: '123456'
-  //       })
-  //       .expect(200)
-  //       .then(async res => {
-  //         const token = res.body.token
-  //         await request(app)
-  //           .post('/api/insert_todo_item')
-  //           .set('x-access-token', token)
-  //           .send({
-  //             idNameTodoArea: 'any_todo_area_name',
-  //             title: 'any_title'
-  //           })
-  //           .expect(400)
-  //         done()
-  //       })
-  //   })
-  // })
+    test('should return 400 on /api/signup missing description', async done => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Gabriel Navas',
+          email: 'gabrielnavas@gmail.com',
+          password: '123456',
+          passwordConfirmation: '123456'
+        })
+        .expect(200)
+        .then(async res => {
+          const token = res.body.token
+          await request(app)
+            .post('/api/insert_todo_item')
+            .set('x-access-token', token)
+            .send({
+              idNameTodoArea: 'any_todo_area_name',
+              title: 'any_title'
+            })
+            .expect(400)
+          done()
+        })
+    })
+  })
 
-  // describe('Expect 403 forbidden', () => {
-  //   beforeEach(async () => {
-  //     await PGHelper.getPool().query('DELETE FROM public."user_token_access" CASCADE')
-  //     await PGHelper.getPool().query('DELETE FROM public."todo_item" CASCADE')
-  //     await PGHelper.getPool().query('DELETE FROM public."user" CASCADE')
-  //   })
+  describe('Expect 403 forbidden', () => {
+    beforeEach(async () => {
+      await PGHelper.getPool().query('DELETE FROM public."user_token_access" CASCADE')
+      await PGHelper.getPool().query('DELETE FROM public."todo_item" CASCADE')
+      await PGHelper.getPool().query('DELETE FROM public."user" CASCADE')
+    })
 
-  //   afterEach(async () => {
-  //     await PGHelper.getPool().query('DELETE FROM public."user_token_access" CASCADE')
-  //     await PGHelper.getPool().query('DELETE FROM public."todo_item" CASCADE')
-  //     await PGHelper.getPool().query('DELETE FROM public."user" CASCADE')
-  //   })
+    afterEach(async () => {
+      await PGHelper.getPool().query('DELETE FROM public."user_token_access" CASCADE')
+      await PGHelper.getPool().query('DELETE FROM public."todo_item" CASCADE')
+      await PGHelper.getPool().query('DELETE FROM public."user" CASCADE')
+    })
 
-  //   test('should return 403 on /api/signup if token not found', async done => {
-  //     const token = 'imATokenFake'
-  //     await request(app)
-  //       .post('/api/insert_todo_item')
-  //       .set('x-access-token', token)
-  //       .send({
-  //         idNameTodoArea: 'any_todo_area_name',
-  //         title: 'any_title'
-  //       })
-  //       .expect(403)
-  //       .then(r => done())
-  //   })
+    test('should return 403 on /api/signup if token not found', async done => {
+      const token = 'imATokenFake'
+      await request(app)
+        .post('/api/insert_todo_item')
+        .set('x-access-token', token)
+        .send({
+          idNameTodoArea: 'any_todo_area_name',
+          title: 'any_title'
+        })
+        .expect(403)
+        .then(r => done())
+    })
 
-  //   test('should return 403 on /api/signup if missing token', async done => {
-  //     await request(app)
-  //       .post('/api/insert_todo_item')
-  //       .send({
-  //         idNameTodoArea: 'any_todo_area_name',
-  //         title: 'any_title'
-  //       })
-  //       .expect(403)
-  //       .then(r => done())
-  //   })
-  // })
+    test('should return 403 on /api/signup if missing token', async done => {
+      await request(app)
+        .post('/api/insert_todo_item')
+        .send({
+          idNameTodoArea: 'any_todo_area_name',
+          title: 'any_title'
+        })
+        .expect(403)
+        .then(r => done())
+    })
+  })
 })
--- tests/main/routes/update-todo-item.test.ts
+++ tests/main/routes/update-todo-item.test.ts
@@ -4,7 +4,7 @@ import app from '@/main/configs/app'
 import { PGHelper } from '@/infra/db/postgresql/helpers/pg-helper'
 
 describe('POST /update_todo_item', () => {
-  describe('Expect success 200', () => {
+  describe('Expected success 200', () => {
     beforeEach(async () => {
       await PGHelper.getPool().query('DELETE FROM public."user_token_access" CASCADE')
       await PGHelper.getPool().query('DELETE FROM public."todo_item" CASCADE')
@@ -62,6 +62,234 @@ describe('POST /update_todo_item', () => {
       expect(respTodoUpdated.body.idNameTodoArea).toEqual('any_id_todo_area')
       expect(respTodoUpdated.body.title).toEqual('any_title')
       expect(respTodoUpdated.body.description).toEqual('any_description')
+    })
+  })
+
+  describe('Expect 400 badRequest', () => {
+    beforeEach(async () => {
+      await PGHelper.getPool().query('DELETE FROM public."user_token_access" CASCADE')
+      await PGHelper.getPool().query('DELETE FROM public."todo_item" CASCADE')
+      await PGHelper.getPool().query('DELETE FROM public."user" CASCADE')
+    })
+
+    afterEach(async () => {
+      await PGHelper.getPool().query('DELETE FROM public."user_token_access" CASCADE')
+      await PGHelper.getPool().query('DELETE FROM public."todo_item" CASCADE')
+      await PGHelper.getPool().query('DELETE FROM public."user" CASCADE')
+    })
+
+    test('should return 400 on /api/signup missing idTodoItem', async done => {
+      const createNewUserAccount = () => {
+        return request(app)
+          .post('/api/signup')
+          .send({
+            name: 'Gabriel Navas',
+            email: 'gabrielnavas@gmail.com',
+            password: '123456',
+            passwordConfirmation: '123456'
+          })
+          .expect(200)
+          .then(r => r)
+      }
+      const responseSignup = await createNewUserAccount()
+      const token = responseSignup.body.token
+      const respTodoUpdated = await request(app)
+        .post('/api/update_todo_item')
+        .set('x-access-token', token)
+        .send({
+          idNameTodoArea: 'any_id_todo_area',
+          title: 'any_title',
+          description: 'any_description'
+        })
+        .then(r => {
+          return r
+        })
+      done()
+      expect(respTodoUpdated.status).toEqual(400)
+    })
+
+    test('should return 400 on /api/signup missing idNameTodoArea', async done => {
+      const createNewUserAccount = () => {
+        return request(app)
+          .post('/api/signup')
+          .send({
+            name: 'Gabriel Navas',
+            email: 'gabrielnavas@gmail.com',
+            password: '123456',
+            passwordConfirmation: '123456'
+          })
+          .expect(200)
+          .then(r => r)
+      }
+      const insertNewTodoItem = () => {
+        return request(app)
+          .post('/api/insert_todo_item')
+          .set('x-access-token', token)
+          .send({
+            idNameTodoArea: 'any_id_todo_area',
+            title: 'any_title',
+            description: 'any_description'
+          })
+          .expect(200)
+          .then(r => r)
+      }
+      const responseSignup = await createNewUserAccount()
+      const token = responseSignup.body.token
+      const respTodoInserted = await insertNewTodoItem()
+      const respTodoUpdated = await request(app)
+        .post('/api/update_todo_item')
+        .set('x-access-token', token)
+        .send({
+          idTodoItem: respTodoInserted.body.id,
+          title: 'any_title',
+          description: 'any_description'
+        })
+        .then(r => {
+          return r
+        })
+      done()
+      expect(respTodoUpdated.status).toEqual(400)
+    })
+
+    test('should return 400 on /api/signup missing title', async done => {
+      const createNewUserAccount = () => {
+        return request(app)
+          .post('/api/signup')
+          .send({
+            name: 'Gabriel Navas',
+            email: 'gabrielnavas@gmail.com',
+            password: '123456',
+            passwordConfirmation: '123456'
+          })
+          .expect(200)
+          .then(r => r)
+      }
+      const insertNewTodoItem = () => {
+        return request(app)
+          .post('/api/insert_todo_item')
+          .set('x-access-token', token)
+          .send({
+            idNameTodoArea: 'any_id_todo_area',
+            title: 'any_title',
+            description: 'any_description'
+          })
+          .expect(200)
+          .then(r => r)
+      }
+      const responseSignup = await createNewUserAccount()
+      const token = responseSignup.body.token
+      const respTodoInserted = await insertNewTodoItem()
+      const respTodoUpdated = await request(app)
+        .post('/api/update_todo_item')
+        .set('x-access-token', token)
+        .send({
+          idTodoItem: respTodoInserted.body.id,
+          idNameTodoArea: 'any_id_todo_area',
+
+          description: 'any_description'
+        })
+        .then(r => {
+          return r
+        })
+      done()
+      expect(respTodoUpdated.status).toEqual(400)
+    })
+
+    test('should return 400 on /api/signup missing description', async done => {
+      const createNewUserAccount = () => {
+        return request(app)
+          .post('/api/signup')
+          .send({
+            name: 'Gabriel Navas',
+            email: 'gabrielnavas@gmail.com',
+            password: '123456',
+            passwordConfirmation: '123456'
+          })
+          .expect(200)
+          .then(r => r)
+      }
+      const insertNewTodoItem = () => {
+        return request(app)
+          .post('/api/insert_todo_item')
+          .set('x-access-token', token)
+          .send({
+            idNameTodoArea: 'any_id_todo_area',
+            title: 'any_title',
+            description: 'any_description'
+          })
+          .expect(200)
+          .then(r => r)
+      }
+      const responseSignup = await createNewUserAccount()
+      const token = responseSignup.body.token
+      const respTodoInserted = await insertNewTodoItem()
+      const respTodoUpdated = await request(app)
+        .post('/api/update_todo_item')
+        .set('x-access-token', token)
+        .send({
+          idTodoItem: respTodoInserted.body.id,
+          idNameTodoArea: 'any_id_todo_area',
+          title: 'any_title'
+        })
+        .then(r => {
+          return r
+        })
+      done()
+      expect(respTodoUpdated.status).toEqual(400)
+    })
+  })
+
+  describe('Expect 403 forbidden', () => {
+    beforeEach(async () => {
+      await PGHelper.getPool().query('DELETE FROM public."user_token_access" CASCADE')
+      await PGHelper.getPool().query('DELETE FROM public."todo_item" CASCADE')
+      await PGHelper.getPool().query('DELETE FROM public."user" CASCADE')
+    })
+
+    afterEach(async () => {
+      await PGHelper.getPool().query('DELETE FROM public."user_token_access" CASCADE')
+      await PGHelper.getPool().query('DELETE FROM public."todo_item" CASCADE')
+      await PGHelper.getPool().query('DELETE FROM public."user" CASCADE')
+    })
+    test('should return 403 on UpdateOneTodoItem with correct params', async done => {
+      const createNewUserAccount = () => {
+        return request(app)
+          .post('/api/signup')
+          .send({
+            name: 'Gabriel Navas',
+            email: 'gabrielnavas@gmail.com',
+            password: '123456',
+            passwordConfirmation: '123456'
+          })
+          .expect(200)
+          .then(r => r)
+      }
+      const insertNewTodoItem = () => {
+        return request(app)
+          .post('/api/insert_todo_item')
+          .set('x-access-token', token)
+          .send({
+            idNameTodoArea: 'any_id_todo_area',
+            title: 'any_title',
+            description: 'any_description'
+          })
+          .expect(200)
+          .then(r => r)
+      }
+      const responseSignup = await createNewUserAccount()
+      const token = responseSignup.body.token
+      const respTodoInserted = await insertNewTodoItem()
+      const respTodoUpdated = await request(app)
+        .post('/api/update_todo_item')
+        .send({
+          idTodoItem: respTodoInserted.body.id,
+          idNameTodoArea: 'any_id_todo_area',
+          title: 'any_title',
+          description: 'any_description'
+        })
+        .then(r => r)
+      done()
+      expect(respTodoUpdated.status).toEqual(403)
     })
   })
 })
